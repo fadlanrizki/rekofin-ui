d9314d2015ee435b92394411dbf7d90d781a3ae1
Fadlan Rizki <66633252+fadlanrizki@users.noreply.github.com>
2025-07-13 00:47:47 +0700

    fix: quote typography

--- src/components/views/LandingPage/Sections/Quote.tsx
+++ src/components/views/LandingPage/Sections/Quote.tsx
@@ -78,9 +78,9 @@ const Quote = () => {
         {quotes.map((quote: QuoteType, index) => (
           <SwiperSlide key={index}>
             <div className="flex flex-col m-auto w-full px-10 md:w-1/2 items-center justify-evenly gap-5 text-white py-20 box-border">
-              <p className="italic text-gray-700 text-center">“{quote.text}”</p>
+              <p className="text-gray-700 text-center text-2xl md:text-xl italic font-light">“{quote.text}”</p>
               <div className="text-center">
-                <p className="font-bold text-blue-900 mt-4">{quote.author}</p>
+                <p className="text-lg text-primary md:text-base font-semibold mt-2">{quote.author}</p>
                 <p className="text-sm text-gray-500">{quote.role}</p>
               </div>
             </div>
