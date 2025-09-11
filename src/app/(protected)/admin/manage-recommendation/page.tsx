ddc664977e983e34c18b67778c1783be74215656
fadlanrizky19 <fadlanrizky19@gmail.com>
2025-09-11 11:47:18 +0700

    fix: delete unused protected route

--- src/app/(protected)/admin/manage-recommendation/page.tsx
+++ src/app/(protected)/admin/manage-recommendation/page.tsx
@@ -1,11 +1,6 @@
-import ProtectedRoute from "@/components/ProtectedRoute";
 import ManageRecommendationView from "@/components/views/ManageRecommendation/ManageRecommendationView";
 import React from "react";
 
 export default function ManageRecommendationPage() {
-  return (
-    <ProtectedRoute>
-      <ManageRecommendationView />
-    </ProtectedRoute>
-  );
+  return <ManageRecommendationView />;
 }
--- src/app/(protected)/user/finance/page.tsx
+++ src/app/(protected)/user/finance/page.tsx
@@ -1,12 +1,7 @@
-import ProtectedRoute from "@/components/ProtectedRoute";
 import FinanceView from "@/components/views/Finance/FinanceView";
 
 const FinancePage = () => {
-  return (
-    <ProtectedRoute>
-      <FinanceView />
-    </ProtectedRoute>
-  );
+  return <FinanceView />;
 };
 
 export default FinancePage;
