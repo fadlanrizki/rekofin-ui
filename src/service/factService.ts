b0bf66e48dde6eb9ac2aa1be32bd7c0c618c1ff5
fadlanrizky19 <fadlanrizky19@gmail.com>
2026-01-14 23:05:52 +0700

    feat: update progress

---
+++ src/service/factService.ts
@@ -0,0 +1,33 @@
+import { API_ROUTE } from "@/utils/constants/api-routes";
+import { apiClient } from "./apiService";
+import { ParamsUser } from "@/types/user";
+
+const create = async (data: any): Promise<any> => {
+  return await apiClient.post(API_ROUTE.FACT, data);
+};
+
+const getList = async (params: ParamsUser): Promise<any> => {
+  return await apiClient.get(API_ROUTE.FACT, params);
+};
+
+const deleteData = async (id: string): Promise<any> => {
+  const url = `${API_ROUTE.FACT}/${id}`;
+  return await apiClient.delete(url);
+};
+
+const findById = async (id: string): Promise<any> => {
+  const url = `${API_ROUTE.FACT}/${id}`;
+  return await apiClient.get(url);
+};
+
+const update = async (data: any): Promise<any> => {
+  return await apiClient.patch(API_ROUTE.FACT, data);
+};
+
+export const FactService = {
+  create,
+  getList,
+  deleteData,
+  findById,
+  update
+};
